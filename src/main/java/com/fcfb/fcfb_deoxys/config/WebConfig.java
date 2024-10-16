cd95191962db71dc42e56542178269ed585001c6
Andrew Kicklighter <apkicklighter@gmail.com>
2024-10-16 16:01:19 -0500

    add options request

--- src/main/java/com/fcfb/fcfb_deoxys/config/WebConfig.java
+++ src/main/java/com/fcfb/fcfb_deoxys/config/WebConfig.java
@@ -1,15 +1,11 @@
 package com.fcfb.fcfb_deoxys.config;
 
+import com.fcfb.fcfb_deoxys.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -17,23 +13,12 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("*"));  // You can specify allowed origins here
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        return new CorsFilter();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()  // Enable CORS
-                .and()
                 .authorizeRequests()
                 .antMatchers("/deoxys/**")
                 .permitAll()
